e789c9dce9f9126a19e771c58be1a887d2f2d444
GilanRanger <58899528+GilanRanger@users.noreply.github.com>
2020-09-05 15:57:12 -0700

    New NameHistoryUtil

--- src/main/java/com/mcmiddleearth/mcmechat/util/NameHistoryUtil.java
+++ src/main/java/com/mcmiddleearth/mcmechat/util/NameHistoryUtil.java
@@ -19,161 +19,134 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.*;
 
-public class NameHistoryUtil implements CommandExecutor {
+public class NameHistoryUtil{
 
-    @Override
-    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (args.length > 0) {
-            if (sender instanceof Player) {
-                //Name (Args[0])
-                String name = args[0];
-
-                //Get UUID
-                URL url = null;
-                try {
-                    url = new URL("https://api.mojang.com/users/profiles/minecraft/" + name);
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                }
-
-                Scanner sc = null;
-                try {
-                    sc = new Scanner(url.openStream());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                StringBuilder sb = new StringBuilder();
-                while (sc.hasNext()) {
-                    sb.append(sc.next());
-
-                }
-
-                String result = sb.toString();
-                int start = result.lastIndexOf(":") + 2;
-                int end = start + 32;
-                String PlayerUUID = result.substring(start, end);
-
-                //Get all names from UUID
-                URL url1 = null;
-                try {
-                    url1 = new URL("https://api.mojang.com/user/profiles/" + PlayerUUID + "/names");
-                } catch (MalformedURLException e) {
-                    e.printStackTrace();
-                }
-
-                Scanner sc1 = null;
-                try {
-                    sc1 = new Scanner(url1.openStream());
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-
-                StringBuilder sb1 = new StringBuilder();
-                while (sc1.hasNext()) {
-                    sb1.append(sc1.next());
-
-                }
-                
-                //Create 2 lists for names and dates
-                String result1 = sb1.toString();
-                List<String> names = new ArrayList<>();
-                List<String> dates = new ArrayList<>();
-
-                //NAMES
-                int length = result1.length();
-                int stringPos = 0;
-                for (int i = 0; i < length; i++) {
-                    char current = result1.charAt(stringPos);
-                    //Checks if at beginning of section
-                    if (current == 123) {
-                        int start1 = stringPos + 9;
-                        int count = 9;
-                        while (result1.charAt(stringPos + count) != 125) {
-                            if (result1.charAt(stringPos + count) == 58) {
-                                int end1 = stringPos + count;
-                                String name1 = result1.substring(start1, end1);
-                                //Get rid of symbols that are not needed
-                                name1 = name1.replace("\"", "");
-                                name1 = name1.replace(",", "");
-                                name1 = name1.replaceAll("changedToAt", "");
-                                names.add(name1);
-
-                            }
-                            count++;
-                        }
-                    }
-                    stringPos++;
-                }
-
-                int dateLength = result1.length();
-                int datePos = 0;
-
-                //DATES
-                for (int i = 0; i < dateLength; i++) {
-                    char current = result1.charAt(datePos);
-                    if (current == 58) {
-                        int num = datePos + 1;
-                        if (result1.charAt(num) > 47 && result1.charAt(num) < 58) {
-                            int start1 = num;
-                            int end1 = num + 13;
-                            String date = result1.substring(start1, end1);
-                            long milliSec = Long.parseLong(date);
-                            //Convert milliseconds to a Date
-                            DateFormat simple = new SimpleDateFormat("dd MMM yyyy");
-                            Date newDate = new Date(milliSec);
-                            dates.add(simple.format(newDate));
-
-                        }
-                    }
-                    datePos++;
-                }
-
-                //Adds in the original name (this does not have a date)
-                int firstIndex = result1.indexOf("},");
-                int start1 = 10;
-                int end1 = firstIndex - 1;
-                String name1 = result1.substring(start1, end1);
-                names.add(0, name1);
-                dates.add(0, "Account Created");
-
-                int size = names.size();
-                //Creates Fancy message for header
-                FancyMessage header = new FancyMessage(MessageType.INFO,
-                        MCMEtesting.getMessageUtil())
-                        .addSimple(ChatColor.DARK_AQUA + "Player Name History ");
-                //Creates FancyList for names and dates
-                List<FancyMessage> fancyList = new ArrayList<>();
-
-                for (int i = 0; i < size; i++) {
-                    FancyMessage element = new FancyMessage(MessageType.INFO, MCMEtesting.getMessageUtil())
-                            .addSimple(names.get(i) + ", " + dates.get(i));
-                    fancyList.add(element);
-                }
-                //Checks which page was entered
-                int page;
-                if(args[1]!=null){
-                if (args[1].length() > 0 && NumericUtil.isInt(args[1])) {
-                    page = NumericUtil.getInt(args[1]);
-                    //Sends name history message to sender    
-                    try {
-                            MCMEtesting.getMessageUtil().sendFancyListMessage((Player) sender, header,
-                                    fancyList,
-                                    "/namehistory", page);
-                        }catch (NullPointerException e) {
-                            e.printStackTrace();
-                            sender.sendMessage(ChatColor.DARK_RED + "Please include player name and page number: /namehistory <name> <page>");
-                        }
-                    }
-                }else{
-                    sender.sendMessage(ChatColor.DARK_RED + "Please include player name and page number: /namehistory <name> <page>");
-                }
-
-            }
-        }else if(args[0].length() == 0 && args[1].length() == 0){
-            sender.sendMessage(ChatColor.DARK_RED + "Please include player name and page number: /namehistory <name> <page>");
+     public static List<FancyMessage> getFancyList(String name) {
+        //Get UUID
+        URL url = null;
+        try {
+            url = new URL("https://api.mojang.com/users/profiles/minecraft/" + name);
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
         }
-        return true;
+
+        Scanner sc = null;
+        try {
+            sc = new Scanner(url.openStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        StringBuilder sb = new StringBuilder();
+        while (sc.hasNext()) {
+            sb.append(sc.next());
+
+        }
+
+        String result = sb.toString();
+        int start = result.lastIndexOf(":") + 2;
+        int end = start + 32;
+        String playerUUID = result.substring(start, end);
+
+        //Get all names from UUID
+        URL url1 = null;
+        try {
+            url1 = new URL("https://api.mojang.com/user/profiles/" + playerUUID + "/names");
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        }
+
+        Scanner sc1 = null;
+        try {
+            sc1 = new Scanner(url1.openStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        StringBuilder sb1 = new StringBuilder();
+        while (sc1.hasNext()) {
+            sb1.append(sc1.next());
+
+        }
+
+        String info = sb1.toString();
+
+
+        //NAMES
+        List<String> names = new ArrayList<>();
+
+        int length = info.length();
+        int stringPos = 0;
+        for (int i = 0; i < length; i++) {
+            char current = info.charAt(stringPos);
+            //Checks if at beginning of section
+            if (current == 123) {
+                int start1 = stringPos + 9;
+                int count = 9;
+                while (info.charAt(stringPos + count) != 125) {
+                    if (info.charAt(stringPos + count) == 58) {
+                        int end1 = stringPos + count;
+                        String name1 = info.substring(start1, end1);
+                        name1 = name1.replace("\"", "");
+                        name1 = name1.replace(",", "");
+                        name1 = name1.replaceAll("changedToAt", "");
+                        names.add(name1);
+
+                    }
+                    count++;
+                }
+            }
+            stringPos++;
+        }
+
+
+        //DATES
+        List<String> dates = new ArrayList<>();
+
+        int dateLength = info.length();
+        int datePos = 0;
+        for (int i = 0; i < dateLength; i++) {
+            char current = info.charAt(datePos);
+            if (current == 58) {
+                int num = datePos + 1;
+                if (info.charAt(num) > 47 && info.charAt(num) < 58) {
+                    int start1 = num;
+                    int end1 = num + 13;
+                    String date = info.substring(start1, end1);
+                    long milliSec = Long.parseLong(date);
+                    DateFormat simple = new SimpleDateFormat("dd MMM yyyy");
+                    Date newDate = new Date(milliSec);
+                    dates.add(simple.format(newDate));
+
+                }
+            }
+            datePos++;
+        }
+
+        int firstIndex = info.indexOf("},");
+        if(firstIndex==-1){
+            firstIndex = info.indexOf("}]");
+        }
+        int start1 = 10;
+        int end1 = firstIndex - 1;
+        String name1 = info.substring(start1, end1);
+        names.add(0, name1);
+        dates.add(0, "Account Created");
+
+
+        int size = names.size();
+
+
+        List<FancyMessage> fancyList = new ArrayList<>();
+
+        for (int i = 0; i < size; i++) {
+            FancyMessage element = new FancyMessage(MessageType.INFO, MCMEtesting.getMessageUtil())
+                    .addSimple(names.get(i) + ", " + dates.get(i));
+            fancyList.add(element);
+        }
+
+
+        return fancyList;
     }
 }
 
